fa13e38491112253e8e67ace944af5c68082f8d7
Brian H <bheptin@gmail.com>
2018-05-04 15:33:06 -0500

    cleanup and add _inputElement

--- src/message.js
+++ src/message.js
@@ -4,21 +4,36 @@ class Message extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: '',
+      message: [],
+    };
+    this.addMessage = this.addMessage.bind(this);
+  }
+
+
+  addMessage(event) {
+    if (this._inputElement.value !== "") {
+      var newMessage = {
+        text: this._inputElement.value,
+        key: Date.now()
+      };
+      this.setState((prevState) => {
+        return {
+          message: prevState.message.concat(newMessage)
+        };
+      });
     }
-  };
-
-
-  handleClick(event) {
-    console.log(this.state.message);
+    this._inputElement.value = "";
     event.preventDefault();
+    console.log(this.state.message);
   }
 
   render() {
     return(
-      <form  onSubmit={this.handleClick}>
-        <input onChange={event => this.setState({message: event.target.value})} />
-        <button onClick={(e) => this.handleClick(e)}  type="submit">add</button>
+      <form  onSubmit={this.addMessage}>
+        <input
+        ref={(a) => this._inputElement = a}
+        placeholder="Add to the list" />
+        <button type="submit">add</button>
 
       </form>
     );
